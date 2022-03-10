8df5317c9f7a67a7676909b97b21638031f140bc
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-10 18:53:30 +0530

    Uc4 : part time added switch

--- EmployeWage/EmployeWage/Program.cs
+++ EmployeWage/EmployeWage/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to EmployeWage!");
 
-Random random = new Random();
+Random random = new Random(); //it is using to generate random number
 const int absent = 0,
               present = 1,
               halfday = 2,
@@ -10,28 +10,21 @@ const int absent = 0,
               partTime = 4;
 
 
-int isPresent  = random.Next(3), 
+int isPresent  = random.Next(3), // 0= absent , 1=fullTime/present, 2=halfday/partTime 
     empWage = 0;
-
-
-if(isPresent  == present)
+switch ( isPresent )
 {
-    Console.WriteLine(" Employee is Present fullTime ");
-    empWage = ratePerHr * fullTime;
-    int fullTimeHr = 8;
-
-}
-
-else if (isPresent == halfday)
-{
-    Console.WriteLine(" Employee is Present partTime ");
-    empWage = ratePerHr * partTime;
-
-}
-
-else
-{
-    Console.WriteLine(" Employee is Absent"); 
+    case present:
+        Console.WriteLine("Employee is Present fullTime");
+        empWage = ratePerHr * fullTime;
+        break;
+    case halfday:
+        Console.WriteLine(" Employee is Present partTime ");
+        empWage = ratePerHr * partTime;
+        break;
+    default:
+        Console.WriteLine(" Employee is Absent");
+        break;
 }
 
 Console.WriteLine("Employee Wage : "+ empWage);
